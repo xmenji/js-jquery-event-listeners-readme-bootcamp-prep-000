bfa3c2e349a51ca219c912cdfae2cfc38aefcfab
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-28 01:56:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,7 @@ function pressIt(){
 }
 
 function submitIt(){
-  document.on('submit', function(){
+  $('form').on('submit', function(){
     alert('Your form is going to be submitted now.');
   })
 }
